809704cbb25dfebabb56701a4d4182b5649788dc
WCY-dt <834421194@qq.com>
2025-11-27 00:46:54 +0800

    chore: update version to 1.2.4 in changelog, README, package files, and AboutTab component

--- frontend/src/components/modals/settings/AboutTab.vue
+++ frontend/src/components/modals/settings/AboutTab.vue
@@ -41,7 +41,7 @@ const emit = defineEmits<{
   'download-install-update': [];
 }>();
 
-const appVersion: Ref<string> = ref('1.2.3');
+const appVersion: Ref<string> = ref('1.2.4');
 
 onMounted(async () => {
   // Fetch current version from API
